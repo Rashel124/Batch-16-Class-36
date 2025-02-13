7d62b7fbb52a8483761099d2d7c23d275c016bde
Rashel124 <webcoderitrashel124@gmail.com>
2025-02-13 04:06:23 -0800

    Class Test

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
 
+let age = 67;
+
+
+if(age>=60){
+    console.log("the ticket price is $8");
+
+
+}
+
+else if(age>=18){
+    console.log("the ticket price is $15");
+}
+
+
+else if(age>=5){
+    console.log("the ticket price is $10");
+}
+
+else{
+    console.log("the ticket price is free");
+}
